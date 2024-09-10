e9174a4ccecf8ccac627440fd5ac7215ecc70d4a
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-09-10 13:31:43 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,30 @@
-const message = "Hello World";
-console.log(message);
+// ========== Example 2 ==========
+
+// import path from 'node:path';
+// const workDir = path.join(process.cwd(), 'src');
+// const somePath = path.join('some_folder.txt', 'some_folder.txt');
+
+// console.log(somePath);
+// console.log(workDir);
+
+// ========== Example 3 ==========
+
+import fs from 'fs/promises';
+import path from 'path';
+
+const filePath = path.join(process.cwd(), 'src', 'text.txt');
+
+// const fileContentPromise = await fs.readFile(filePath);
+// console.log(fileContentPromise);
+
+(async () => {
+  try {
+    const text = 'Hello World!';
+    await fs.writeFile('output.txt', text);
+    console.log('Text successfully written to file');
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+// 02:04:00
